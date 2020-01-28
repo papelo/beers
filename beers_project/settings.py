bbbd1ffac7ee55bb4619f21237fc9ade32f81e77
Ariel <ariel.martinez@smartprotection.com>
2020-01-28 18:04:05 +0100

    Changes 2020-01-28

--- beers_project/settings.py
+++ beers_project/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'beers_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'default':  {
+                'ENGINE': 'django.db.backends.mysql',
+                'NAME': 'beers',
+                'USER': 'root',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                'PORT': ''
+            }
 }
+
 
 
 # Password validation
